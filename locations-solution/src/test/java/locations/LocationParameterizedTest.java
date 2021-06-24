1347c6ee8be26448826e680f6310f3bda0e7fb8a
kondasg <gabor@kondas.hu>
2021-06-24 23:02:56 +0200

    Szoftvertesztelés Java platformon - gyakorlati feladatok / Paraméterezett tesztek

---
+++ locations-solution/src/test/java/locations/LocationParameterizedTest.java
@@ -0,0 +1,41 @@
+package locations;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class LocationParameterizedTest {
+
+    LocationParser locationParser = new LocationParser();
+
+    @ParameterizedTest
+    @MethodSource("createLocations")
+    void testIsOnPrimeMeridian(Location location, boolean expected) {
+        assertEquals(expected, locationParser.isOnPrimeMeridian(location));
+    }
+
+    static Stream<Arguments> createLocations() {
+        return Stream.of(
+                arguments(new Location("p1", 47.497912, 19.040235), false),
+                arguments(new Location("p2", 47.497912, 0), true),
+                arguments(new Location("p3", 12, 19.040235), false),
+                arguments(new Location("p4", 47.497912, 0), true),
+                arguments(new Location("p5", 0, 21), false),
+                arguments(new Location("p6", 47.497912, 19.040235), false)
+        );
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    void testDistanceFromCsvFile(String lat1, String lon1, String lat2, String lon2, double distance) {
+        Location location1 = locationParser.parse("," + lat1 + "," + lon1);
+        Location location2 = locationParser.parse("," + lat2 + "," + lon2);
+        assertEquals(distance, location1.distanceFrom(location2) * 1000, 0.1);
+    }
+}
