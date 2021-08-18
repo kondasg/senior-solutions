bb576c0f0e988ebae4befdc8feb31a1a478e12f0
kondasg <gabor@kondas.hu>
2021-08-18 20:57:53 +0200

    init

---
+++ tasks/sv2021-jvjbf-reszvizsga-pot/src/main/java/booking/Sv2021JvjbfReszvizsgaPotApplication.java
@@ -0,0 +1,13 @@
+package booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sv2021JvjbfReszvizsgaPotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sv2021JvjbfReszvizsgaPotApplication.class, args);
+    }
+
+}
